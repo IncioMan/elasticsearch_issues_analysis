a50d5419833b90c8a2b0a62f42fba2220f7cd709
Ubuntu <azureuser@elasticvm.vmwhjhir3shetmlkxjsgvyk4ke.bx.internal.cloudapp.net>
2020-08-29 17:11:49 +0000

    parameter

--- versiontest.sh
+++ versiontest.sh
@@ -1,18 +1,20 @@
+#|/bin/bash
 cd $1
 if ! [ -d $2 ] 
 then
     echo "Creating folder $2..."
     mkdir $2
 fi
-for i in {1..1}
+for i in $(seq 1 $5);
 do
-    if [ -d "$2/$i" ] 
-    then
-        echo "Deleting folder $2/$i..."
-        rm -r $2/$i/
-    echo "Creating folder $2/$i..."
-    mkdir $2/$i    
-    fi
+  echo $i    
+  if [ -d "$2/$i" ] 
+  then
+      echo "Deleting folder $2/$i..."
+      rm -rf $2/$i/
+  echo "Creating folder $2/$i..."
+  mkdir $2/$i    
+  fi
     docker run -it -w /elasticsearch/ -v "$1/$2/$i:/tmp/test_reports/" -v "$4/.gradle/wrapper:/root/.gradle/wrapper" -v "$4/.gradle/caches:/root/.gradle/caches" elastic/$3 /bin/bash -c "git checkout tags/$2 -b $2 &&\
     ./gradlew --stop && ./gradlew :server:test --build-cache --tests org.apache.lucene.queries.DoubleRandomBinaryDocValuesRangeQueryTests &&\
     cp -r /elasticsearch/server/build/reports/tests/test /tmp/test_reports"
