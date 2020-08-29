0c2dc5d5bfab7476cd69f77396c54fa1af749e98
Alex Incerti <alexincerti@Alexs-MacBook-Pro.local>
2020-08-29 18:31:46 +0200

    fix

--- versiontest.sh
+++ versiontest.sh
@@ -1,5 +1,4 @@
 cd $1
-echo "ciao $1"
 if ! [ -d $2 ] 
 then
     echo "Creating folder $2..."
@@ -7,13 +6,12 @@ then
 fi
 for i in {1..1}
 do
-    if ! [ -d "$2/$i" ] 
+    if [ -d "$2/$i" ] 
     then
-        echo "Creating folder $2/$i..."
-        mkdir $2/$i
-    else
-        echo "Emptying folder $2/$i..."
-        rm $2/$i/*
+        echo "Deleting folder $2/$i..."
+        rm -r $2/$i/
+    echo "Creating folder $2/$i..."
+    mkdir $2/$i    
     fi
     docker run -it -w /elasticsearch/ -v "$1/$2/$i:/tmp/test_reports/" -v "$4/.gradle/wrapper:/root/.gradle/wrapper" -v "$4/.gradle/caches:/root/.gradle/caches" elastic/$3 /bin/bash -c "git checkout tags/$2 -b $2 &&\
     ./gradlew --stop && ./gradlew :server:test --build-cache --tests org.apache.lucene.queries.DoubleRandomBinaryDocValuesRangeQueryTests &&\
